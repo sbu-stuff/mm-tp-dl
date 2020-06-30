117ffd6c87cbd1d6fba385ef8914c6a6b455eabe
unknown <samlew5@yahoo.com>
2020-06-30 17:32:48 -0400

    a few tests

--- netlists/writeNetlist.m
+++ netlists/writeNetlist.m
@@ -8,28 +8,29 @@ function [] = writeNetlist(n_cells,file_name,pm)
 
   if ~exist('pm')
     pm.V = 'PULSE(0 1 0 7.5n 7.5n 100n 1 1)';
-    pm.R = '0.17';
+    pm.Ri = '0.17';
+    pm.Ro = '220';
     pm.L = '290n';
     pm.C = '6.8p';
     pm.PC = '0';
   end
 
-  netlist = sprintf(['*Auto-gererated %d-cell delay line circuit\n\nV1 1 0 ',pm.V,...
-      '\nR1 1 0 220'],n_cells);
-  counts = [1 1 2]; %[C, L, R]
+  netlist = sprintf(['*Auto-gererated %d-cell delay line circuit\n\nV1 1 0 ',...
+      pm.V],n_cells);
+  counts = [1 0 0]; %[C, L, Ri]
   nodes = [[1:2:2*n_cells-1],-1]; %node -1 will represent output
   
   for node = nodes(1:end-1)
     counts += 1;
     C = sprintf(['\nC%d %d 0 ',pm.C,' Cpar = ',pm.PC],counts(1),node);
     L = sprintf(['\nL%d %d %d ',pm.L],counts(2),node,node+1);
-    R = sprintf(['\nR%d %d %d ',pm.R],counts(3),node+1,nodes(counts(1))); %re-using counts b/c it is already 1 index ahead of node
-    netlist = [netlist,C,L,R];
+    Ri = sprintf(['\nR%d %d %d ',pm.Ri],counts(3),node+1,nodes(counts(1))); %re-using counts b/c it is already 1 index ahead of node
+    netlist = [netlist,C,L,Ri];
   end
   
   counts += 1;
-  netlist = [netlist,sprintf(['\nC%d -1 0 ',pm.C,' Cpar = ',pm.PC,'\nR%d -1 0 220',...
-      '\n\n.tran %dn'],counts(1),counts(3),200+ceil((100/64)*n_cells))];
+  netlist = [netlist,sprintf(['\nC%d -1 0 ',pm.C,' Cpar = ',pm.PC,'\nR%d -1 0 ',pm.Ro,...
+      '\n\n.tran %dn'],counts(1),counts(3),200+ceil((100/64)*n_cells+2700))];
       
   fID = fopen(file_name,'w');
   fprintf(fID,netlist);
