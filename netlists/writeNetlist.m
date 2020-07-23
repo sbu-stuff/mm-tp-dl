608d41a4fef9c37936bc14c0daa8c3fc929bce9b
unknown <samlew5@yahoo.com>
2020-07-23 09:10:34 -0400

    tests

--- netlists/writeNetlist.m
+++ netlists/writeNetlist.m
@@ -1,6 +1,6 @@
 %writes to a text file in the format of an LTspice netlist
 
-function [] = writeNetlist(n_cells,file_name,pm)
+function [] = writeNetlist(n_cells,file_name,use_filter,pm)
 %n_cells is the number of delay line cells.  file_name must come in the format of ('___.cir')
 %pm is a struct detailing parameters of the circuit. All elements must be strings.
 %pm: [V,R,L,C,PC] -> [input voltage,cell resistance,cell inductance,cell capacitance,parallel (parasitic) capacitance]
@@ -27,10 +27,19 @@ function [] = writeNetlist(n_cells,file_name,pm)
     Ri = sprintf(['\nR%d %d %d ',pm.Ri],counts(3),node+1,nodes(counts(1))); %re-using counts b/c it is already 1 index ahead of node
     netlist = [netlist,C,L,Ri];
   end
-  
   counts += 1;
-  netlist = [netlist,sprintf(['\nC%d -1 0 ',pm.C,' Cpar = ',pm.PC,'\nR%d -1 0 ',pm.Ro,...
+  
+  if use_filter
+    netlist = [netlist,sprintf(['\nC%d -1 0 ',pm.C,' Cpar = ',pm.PC,'\nR%d -1 0 ',pm.Ro,...
+        '\nRf0 f1 -1 0.1m\nRf1 f3 0 1\nEf1 f3 0 f8 f2 10k\n',...
+        'Rf2 f3 f2 5\nRf3 f2 0 10\nRf4 f8 f7 300\nCf1 f8 0 6.8p\nCf2 f7 0 6.8p\n',...
+        'Rf5 f7 f1 300\nRf6 f-1 0 1\nEf2 f-1 0 f5 f6 10k\nRf7 f-1 f6 5\n',...
+        'Rf8 f6 0 10\nRf9 f5 f4 300\nCf3 f5 0 6.8p\nCf4 f4 0 6.8p\nRf10 f4 f3 300',...
+        '\n\n.tran %dn'],counts(1),counts(3),200+ceil((100/64)*n_cells+200))];
+  else
+    netlist = [netlist,sprintf(['\nC%d -1 0 ',pm.C,' Cpar = ',pm.PC,'\nR%d -1 0 ',pm.Ro,...
       '\n\n.tran %dn'],counts(1),counts(3),200+ceil((100/64)*n_cells+200))];
+  end
       
   fID = fopen(['C:\Users\zOob\Documents\SB REU\LTspice\netlists_\',file_name],'w');
   fprintf(fID,netlist);
--- simulation data/PWL/wnPWL.m
+++ simulation data/PWL/wnPWL.m
@@ -4,7 +4,7 @@ function [] = wnPWL(nfiles,pm,fname,ncells)
   for i = [1:nfiles]
     pm.V = ['V1 1 0 PWL file="C:\\Users\\zOob\\Documents\\SB REU\\LTspice\\data\\',...
         sprintf('PWL_VoltageSource_%d.txt',i),'"'];
-    writeNetlist(ncells,sprintf([fname,'_%d.cir'],i),pm);
+    writeNetlist(ncells,sprintf([fname,'_%d.cir'],i),true,pm);
     pause(0.1);
   end
 end
